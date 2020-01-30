6f6b43fbfd02ac80d3dee1c280309ad4f45f3882
Henrik Panhans <henrik@panhans.dev>
2020-01-30 22:07:35 +0100

    another try

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
         .package(
             url: "https://github.com/apple/swift-package-manager",
-            .revision("swift-5.1.3-RELEASE"))
+            from: "0.5.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
