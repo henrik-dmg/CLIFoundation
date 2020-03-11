47623087987a6bc3a79b8f9077f69eaed5568ced
Henrik Panhans <henrik@panhans.dev>
2020-03-11 14:14:52 +0100

    Fix deployment target

--- Sources/CLIFoundation/NSError+Extensions.swift
+++ Sources/CLIFoundation/NSError+Extensions.swift
@@ -1,7 +1,12 @@
 import Foundation
 
 public extension NSError {
+
     convenience init(code: Int = 1, reason: String) {
-        self.init(domain: "com.henrikpanhans.CLIFoundation", code: code, userInfo: [NSLocalizedFailureErrorKey: reason])
+        self.init(
+            domain: "com.henrikpanhans.CLIFoundation",
+            code: code,
+            userInfo: [NSLocalizedFailureErrorKey: reason])
     }
+
 }
