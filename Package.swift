2ce78d260bd82b769cc99e1cf3b74e273831f384
Henrik Panhans <henrik@panhans.dev>
2020-03-20 12:15:17 +0100

    Added ParsableCommand extension

--- Package.swift
+++ Package.swift
@@ -14,12 +14,15 @@ let package = Package(
             name: "CLIFoundation",
             targets: ["CLIFoundation"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.0.1")
+    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "CLIFoundation",
-            dependencies: []),
+            dependencies: ["ArgumentParser"]),
         .testTarget(
             name: "CLIFoundationTests",
             dependencies: ["CLIFoundation"]),
