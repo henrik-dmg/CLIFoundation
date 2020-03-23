4af0d7d5138854582a3e27a55ca413e0cac7025a
Henrik Panhans <henrik@panhans.dev>
2020-03-23 18:13:48 +0100

    Library has to explicitly specified lol

--- Package.swift
+++ Package.swift
@@ -8,6 +8,9 @@ let package = Package(
     platforms: [
         .macOS(.v10_13)
     ],
+    products: [
+        .library(name: "CLIFoundation", targets: ["CLIFoundation"])
+    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
