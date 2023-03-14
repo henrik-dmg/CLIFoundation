c079257be38b1bbf1fb269464a2dc185153542f2
Henrik Panhans <henrik@panhans.dev>
2023-03-14 16:42:19 +0100

    Add CI workflow files
    Signed-off-by: Henrik Panhans <henrik@panhans.dev>

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.4
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -10,6 +10,10 @@ let package = Package(
     ],
     products: [
         .library(name: "CLIFoundation", targets: ["CLIFoundation"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-docc-plugin", branch: "main"),
+        .package(url: "https://github.com/apple/swift-format", branch: "main")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
