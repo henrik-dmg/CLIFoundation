30879102cd4fa2e78cb6c767fc3e10c6382904f3
Henrik Panhans <henrik@panhans.dev>
2024-01-23 09:37:55 +0100

    Fix documentation compilation

--- Package.swift
+++ Package.swift
@@ -3,11 +3,20 @@
 
 import PackageDescription
 
+#if os(OSX)
+let dependencies: [Package.Dependency] = [
+    .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.1.0"),
+]
+#else
+let dependencies: [Package.Dependency] = []
+#endif
+
 let package = Package(
     name: "CLIFoundation",
     products: [
         .library(name: "CLIFoundation", targets: ["CLIFoundation"])
     ],
+    dependencies: dependencies,
     targets: [
         .target(
             name: "CLIFoundation",
